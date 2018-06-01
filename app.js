3f1fffbadfd3823328de6f16db9a4ce72217af05
Shreeviknesh <shreeviknesh@gmail.com>
2018-06-02 02:44:44 +0530

    Major Changes - Revert from EJS to HTML

--- app.js
+++ app.js
@@ -2,29 +2,15 @@ const express = require('express');
 const app = express();
 
 const port = 5500;
-const eventRouter = require('./src/routes/eventroutes');
 
 app.use(express.static('public'));
 app.use(express.static('bower_components'));
-
-app.set('views', './src/views');
-app.set('view engine', 'ejs')
-
-app.use('/Events', eventRouter);
+app.use(express.static('src/views'));
 
 app.get('/', (req, res) => {
-    res.render('index', {
-        nav: [
-            { Link: 'Services', Text: 'Services'},
-            { Link: 'Portfolio', Text: 'Portfolio'},
-            { Link: 'About', Text: 'About'},
-            { Link: 'Team', Text: 'Team'},
-            { Link: 'Contact', Text: 'Contact'},
-            { Link: 'Events', Text: 'Events'}
-        ]
-    });
-})
+    res.render('index');
+});
 
 app.listen(port, err => {
     console.log(`Server Running on Port:${port}`);
-})
+});
